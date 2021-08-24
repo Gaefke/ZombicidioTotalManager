0db239cbf441dba771e63fbbed57fb2476883c00
Gaefke <gaefkemarcio@gmail.com>
2021-08-23 21:47:37 -0300

    GestãoBunkerComponenteLightning

---
+++ force-app/main/default/aura/AdicionaRecursoCriaturaCMP/AdicionaRecursoCriaturaCMPHelper.js
@@ -0,0 +1,66 @@
+({
+	getRecursos : function(component, event) {
+		let recordId = component.get("v.recordId");
+        
+        let action = component.get("c.recuperaRecursos");
+        
+        action.setParams({
+            recordId : recordId
+        });
+        
+        action.setCallback(this, function(response){
+        	let state = response.getState();
+        	let errors = response.getError();
+            
+            if( state === "SUCCESS" ) {
+                let rows = response.getReturnValue();
+                
+                for( var i =0; i < rows.length; i++ ){
+                    var row = rows[i];
+                    row.label = row.Name;
+                    row.value = row.Id;
+                }
+                console.log('rows ::',rows);    
+                
+                if( rows )
+                	component.set("v.recursosLst", rows);
+                
+            } else {
+                console.log('>>> errors:: ', errors);
+            }
+            
+            
+        });
+        $A.enqueueAction(action);
+    },
+    
+    adicionaRecurso : function(component, event) {
+		let recordId 	= component.get("v.recordId");
+    	let recursoId 	= component.get("v.recursoSelecionado");
+        
+        console.log('>>> adicionaRecurso <<<');
+        
+        let action = component.get("c.adicionarRecursoCriatura");
+        
+        action.setParams({
+            criaturaId : recordId,
+    		recursoId : recursoId
+        });
+        
+        action.setCallback(this, function(response){
+        	let state = response.getState();
+        	let errors = response.getError();
+            
+            console.log('>>> state ::',state);
+            console.log('>>> errors ::',errors);
+            if( state === "SUCCESS" ) {
+                console.log('>>> Success');
+            } else {
+                console.log('>>> errors:: ', errors);
+            }
+            
+            
+        });
+        $A.enqueueAction(action);
+    }
+})
